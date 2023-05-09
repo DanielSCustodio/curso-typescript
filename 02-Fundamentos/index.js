f0d5489bf6c5f3f124f53118664c16cf891fbf9d
Daniel Custodio <play.daniel@hotmail.com>
2023-05-09 16:29:10 -0300

    Tipo número

---
+++ 02-Fundamentos/index.js
@@ -0,0 +1,5 @@
+var x = 10.78754;
+console.log(x);
+x = 85.545475;
+console.log(typeof x);
+console.log(x.toPrecision(4));
---
+++ 02-Fundamentos/index.ts
@@ -0,0 +1,9 @@
+let x: number = 10.78754
+
+console.log(x)
+
+x = 85.545475
+
+console.log(typeof x);
+console.log(x.toPrecision(4)) // 4 casas
+
