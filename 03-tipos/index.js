0fe22ba4535de5fd77512846793adb3c7a91849a
Daniel Custodio <play.daniel@hotmail.com>
2023-05-10 17:33:27 -0300

    Outra sintaxe para arrays

--- 03-tipos/index.js
+++ 03-tipos/index.js
@@ -4,3 +4,6 @@ const y = 10;
 let numbers = [1, 2, 3, 52, x];
 numbers.push(y);
 console.log(numbers);
+let nums = [2, 5, 6, 8, 39];
+nums.push(...numbers);
+console.log(nums);
--- 03-tipos/index.ts
+++ 03-tipos/index.ts
@@ -5,4 +5,7 @@ let numbers: number[] = [1,2,3,52, x]
 numbers.push(y)
 console.log(numbers);
 
+let nums: Array<number> = [2,5,6,8,39]
+nums.push(...numbers)
+console.log(nums);
 
