84a344efb97cb29e60a05a75b24bd104b295f905
Daniel Custodio <play.daniel@hotmail.com>
2023-05-10 17:26:21 -0300

    Arrays

---
+++ 03-tipos/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let x = 2;
+const y = 10;
+let numbers = [1, 2, 3, 52, x];
+numbers.push(y);
+console.log(numbers);
---
+++ 03-tipos/index.ts
@@ -0,0 +1,8 @@
+let x:number = 2
+const y:number = 10
+
+let numbers: number[] = [1,2,3,52, x]
+numbers.push(y)
+console.log(numbers);
+
+
